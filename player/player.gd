7f370a05b51a4ba5c006e6f7938ae5c52d0ff5b7
MacNikSipeykin <niksipeykin@gmail.com>
2025-07-20 16:34:50 +0300

    Add enemies (skeleton)

--- player/player.gd
+++ player/player.gd
@@ -4,12 +4,14 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 @onready var anim = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += gravity * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
